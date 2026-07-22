f235c5a16e93e58e2fa0839d7faa941aac10872a
yashif khan <yashifyashifkhan@gmail.com>
2026-07-22 23:12:17 +0530

    learn the operator in python

---
+++ opraters/index.py
@@ -0,0 +1,12 @@
+# An operator is a symbol or keyword used to perform an operation on values (called operands).
+
+# Arithmetic Operators
+
+# Operator	Meaning	Example	Output
+# +	Addition	10 + 5	15
+# -	Subtraction	10 - 5	5
+# *	Multiplication	10 * 5	50
+# /	Division	10 / 5	2.0
+# %	Modulus (remainder)	10 % 3	1
+# //	Floor division	10 // 3	3
+# **	Power	2 ** 3	8
