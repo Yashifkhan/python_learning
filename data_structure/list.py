fad547369e24d011b9b317423481094da669a0ed
yashif khan <yashifyashifkhan@gmail.com>
2026-07-25 23:38:52 +0530

    learn the data structure and learn set tuple

--- data_structure/list.py
+++ data_structure/list.py
@@ -27,15 +27,46 @@
 
 # prin the gratest element in array and print index 
 
-l=[1,2,38,6,9]
+# l=[1,2,38,6,9]
 
-max=l[0]
-index=0
-for i in range(len(l)):
-    if l[i] > max:
-        max=l[i]
-        index=i
-print(max)
-print(index)
+# max=l[0]
+# index=0
+# for i in range(len(l)):
+#     if l[i] > max:
+#         max=l[i]
+#         index=i
+# print(max)
+# print(index)
+
+
+
+# find the second largest number 
+# arr=[1,4,2,5,6,3,9]
+
+# max1=arr[0]
+# max2=arr[0]
+
+# for i in arr:
+#     if i > max1:
+#         max2=max1
+#         max1=i
+#     elif max2 >max1:
+#         max2=i
+        
+# print("first larget number",max1)
+# print("second larget number",max2)
+
+
+# check the list is short or not  
+
+l=[1,2,6]
+for i in range(len(l)-1):
+    if l[i] < l[i+1]:
+        continue
+    else:
+        print("Your list is not sort")
+        break
+else:
+    print("Your list is sort")    
 
         
---
+++ data_structure/tuple.py
@@ -0,0 +1,6 @@
+# tuple are immutable mans we can not chnage the value after creation 
+# tuple are allow the duplicat value 
+# tuple are heterogines store multy data type value ,str int flot 
+# tuple have order and acces the itme use indexing 
+
+tp=(1,2,3,4) 
